fix accuracy calculation for network

language_prediction/algs/behavior_cloning.py
@@ -174,7 +174,7 @@ class BehaviorCloning(object):
             metrics['lang_loss'] = lang_loss.item()
             with torch.no_grad():
                 lang_pred = torch.argmax(lang_logits, dim=-1)
-                mask = lang_pred != self.network.lang_pad_idx
+                mask = instruction_labels != self.network.lang_pad_idx
                 accuracy = ((lang_pred == instruction_labels) * mask).sum().item() / mask.sum().item()
                 metrics['lang_accuracy'] = accuracy
         else: